4c165041f0226631e7ef263d4cced64abb1a84cb
claudio-inacio <inacio.unipar@gmail.com>
2026-03-31 00:04:44 -0300

    teste mock info build

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,29 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
-import "./index.css";
-import { Toaster } from "@/shared/components/ui/sonner.tsx";
-import { TooltipProvider } from "@/shared/components/ui/tooltip";
-
-
-
-
+// src/main.tsx
 async function enableMocking() {
-    if (import.meta.env.DEV) {
-        const { worker } = await import("../shared/mocks/browser");
-        await worker.start({
-            onUnhandledRequest: "bypass",
-        });
-    }
+  const shouldMock =
+    import.meta.env.DEV ||
+    import.meta.env.VITE_API_MOCKING === "enabled";
+
+  if (!shouldMock) return;
+
+  const { worker } = await import(".././shared/mocks/browser");
+
+  await worker.start({
+    onUnhandledRequest: "bypass",
+  });
 }
 
 enableMocking().then(() => {
-    ReactDOM.createRoot(document.getElementById("root")!).render(
-        <React.StrictMode>
-            <TooltipProvider>
-                <App />
-            </TooltipProvider>
-            <Toaster />
-        </React.StrictMode>
-    );
+  import("react-dom/client").then(({ createRoot }) => {
+    import("./App").then(({ default: App }) => {
+      const root = document.getElementById("root");
+      if (!root) throw new Error("Root element not found");
+      createRoot(root).render(<App />);
+    });
+  });
 });
